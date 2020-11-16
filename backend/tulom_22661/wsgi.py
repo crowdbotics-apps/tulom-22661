53ea0d82c5abc639de01173670111887d65f9aa3
Crowdbotics <team@crowdbotics.com>
2020-11-16 05:39:46 +0000

    Update models

--- backend/tulom_22661/wsgi.py
+++ backend/tulom_22661/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tulom_22661.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tulom_22661.settings")
 
 application = get_wsgi_application()
